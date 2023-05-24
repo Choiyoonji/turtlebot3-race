10bd37d1cbbb879f481fb4fe0eaaf3cbc9c81efa
choiyoonji <cyj21c6352@gmail.com>
2023-05-24 23:44:09 +0900

    0524 그리드

--- astar.py
+++ astar.py
@@ -5,9 +5,9 @@ from scipy.spatial import distance
 Hp = 1
 Gp = 1
 
-MAP_B = 200
-MAP_H = 600
-TURTLE_SIZE = 2
+MAP_B = 20
+MAP_H = 60
+TURTLE_SIZE = 1
 
 ind = [[0,1],[1,0],[-1,0],[0,-1], [1,1], [-1,1], [1,-1], [-1,-1]]
 
@@ -25,7 +25,7 @@ class Node:
 
 class Astar:
     def __init__(self):
-        self.MAP = np.zeros(MAP_B,MAP_H)
+        self.MAP = np.zeros([MAP_B,MAP_H])
         
         self.open_list = []
         self.close_list = []
@@ -48,25 +48,28 @@ class Astar:
         return node.fcost
     
     def make_map(self,obs_xy):
-        self.MAP = np.zeros(MAP_B,MAP_H)
         for xy in obs_xy:
             for i in range(-TURTLE_SIZE,TURTLE_SIZE):
                 for j in range(-TURTLE_SIZE,TURTLE_SIZE):
                     xd = xy[0] + i
                     yd = xy[1] + j
-                    if xd >= 0 and yd >= 0:
+                    if MAP_B > xd >=0 and MAP_H > yd >= 0:
+                        # print(1)
                         self.MAP[xd,yd] = -1
     
     def GetNewNodes(self):
         for i in ind:
             xd = self.curNode.x - i[0]  
             yd = self.curNode.y - i[1]
-            if xd >=0 and yd >= 0 and self.MAP[xd][yd] == 0:
-                self.MAP[xd][yd] = 2
-                self.open_list.append(Node([xd,yd], self.curNode))
-                self.curNode.cnode.append(Node([xd,yd],self.curNode))
+            if MAP_B > xd >=0 and MAP_H > yd >= 0:
+                xd = int(xd)
+                yd = int(yd)
+                if not self.MAP[xd,yd]:
+                    self.MAP[xd,yd] = 2
+                    self.open_list.append(Node([xd,yd], self.curNode))
+                    self.curNode.cnode.append(Node([xd,yd],self.curNode))
         
-    def generate_path(self, heading, obs_xy):
+    def generate_path(self, obs_xy):
         self.obs = []
         self.obs.extend(obs_xy)
         self.make_map(obs_xy)
@@ -75,13 +78,11 @@ class Astar:
         self.close_list = []
         
         self.xy_start = [MAP_B/2, 0]
-        self.heading = heading
         
-        self.curNode = Node(self.xy_start, self.heading)
+        self.curNode = Node(self.xy_start)
         self.open_list.append(self.curNode)
         
         while self.open_list:
-            
             self.curNode = min(self.open_list, key=lambda n: n.fcost)
             self.close_list.append(self.curNode)
             self.open_list.remove(self.curNode)
@@ -94,7 +95,7 @@ class Astar:
             for n_node in self.open_list:
                 self.calc_fcost(n_node)
             
-            print(len(self.open_list),self.curNode.x,self.curNode.y)
+            # print(len(self.open_list),self.curNode.x,self.curNode.y)
                 
         return self.extracted_path()
 
@@ -111,3 +112,14 @@ class Astar:
             self.curNode = self.curNode.pnode
             
         return path
+
+def main():
+    astar = Astar()
+    obs = [[3,10],[4,10],[5,10],[7,10],[8,10],[9,10],[10,10]]
+    path = astar.generate_path(obs)
+    # ma = np.array(astar.MAP)
+    np.set_printoptions(threshold=np.inf,linewidth=np.inf)
+    print(path)
+
+if __name__ == '__main__':
+    main()
